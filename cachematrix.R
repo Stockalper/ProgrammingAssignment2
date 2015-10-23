9df2e7e39892714665f8fd7b990c39756e18ebbc
Stockalper <big.inukshuk@hotmail.com>
2015-10-23 22:52:49 +0200

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,99 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programming Assignment 2: Lexical Scoping
+# Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+# Task description:
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. 
+# The assignment is to write a pair of functions that cache the inverse of a matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+# The excessive use of comments is for the learning purpose only.
 
+
+
+# 1) makeCacheMatrix 
+# create a list containing 4 subfunctions
+makeCacheMatrix<- function(x = numeric()) {
+  
+  inverse_of_matrix <- NULL
+  
+  # subfunction 1
+  # set the value of the matrix
+  set <- function(y) {
+    x <<- y
+    inverse_of_matrix <<- NULL
+  }
+  
+  # subfunction 2
+  # get the value of the matrix stored in x
+  get <- function() {
+    x
+  }
+  
+  # subfunction 3
+  # take the inverse passed into the function and store it in inverse_of_matrix (the cache)
+  set_inverse_of_matrix <- function(inverse) {
+    inverse_of_matrix <<- inverse
+  }
+  
+  # subfunction 4
+  # get the value of invese_of_matrix (the cache)
+  get_inverse_of_matrix <- function() {
+    inverse_of_matrix
+  }
+  
+  # create list of subfunctions 1-4
+  list(set = set, 
+       get = get,
+       set_inverse_of_matrix = set_inverse_of_matrix,
+       get_inverse_of_matrix = get_inverse_of_matrix)
 }
 
 
-## Write a short comment describing this function
+
+# 2) cacheSolve
+# Retun the inverse of the matrix 
+# condition: x = must be an invertible matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # get content of x's cache
+  inverse_of_matrix <- x$get_inverse_of_matrix()
+  
+  # check if there is content in the cache 
+  # if yes: show message and return content 
+  # if no:  get matrix, invert matrix, save inverted matrix to the cache of x and return inverse of the matrix
+  
+  if(!is.null(inverse_of_matrix)) {
+    message("getting the cached inverse of matrix:")
+    return(inverse_of_matrix) 
+  } else {
+    data <- x$get()
+    inverse_of_matrix <- solve(data, ...)
+    x$set_inverse_of_matrix(inverse_of_matrix)
+    inverse_of_matrix
+  }
 }
+
+
+
+# # Sample code
+# m <- makeCacheMatrix(matrix(c(4,3,3,2),2,2))
+# m$get()
+# # >       [,1] [,2]
+# # > [1,]    4    3
+# # > [2,]    3    2
+# 
+# # first run (nothing in cache)
+# cacheSolve(m)
+# # >       [,1] [,2]
+# # > [1,]   -2    3
+# # > [2,]    3   -4
+# 
+# # second run (return cache)
+# cacheSolve(m)
+# 
+# # > getting the cached inverse of matrix:
+# # >       [,1] [,2]
+# # > [1,]   -2    3
+# # > [2,]    3   -4
+
